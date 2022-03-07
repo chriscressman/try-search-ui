6e3da95b871d05d22869b067892c07adc68e8412
Chris Cressman <chris@chriscressman.com>
2022-03-07 17:11:27 -0500

    Initialize an AppSearchAPIConnector

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector'
+
+const ENTERPRISE_SEARCH_BASE_URL = 'https://try-search-ui.ent.us-central1.gcp.cloud.es.io';
+const APP_SEARCH_SEARCH_KEY = 'search-eaesy8bpwje7u7jgtydgs846';
+const APP_SEARCH_ENGINE_NAME = 'national-parks-demo';
+
+const connectorConfiguration = {
+  endpointBase: ENTERPRISE_SEARCH_BASE_URL,
+  searchKey: APP_SEARCH_SEARCH_KEY,
+  engineName: APP_SEARCH_ENGINE_NAME,
+  cacheResponses: false,
+}
+
+const connector = new AppSearchAPIConnector(connectorConfiguration);
+
 function App() {
   return (
     <div className="App">
