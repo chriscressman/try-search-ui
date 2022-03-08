67d3b60e152d04ef41fbd8640b54fec8ce696db9
Chris Cressman <chris@chriscressman.com>
2022-03-08 16:35:15 -0500

    Start over

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector'
-import { SearchProvider, SearchBox, Results } from '@elastic/react-search-ui'
-import { Layout } from '@elastic/react-search-ui-views'
-
-import '@elastic/react-search-ui-views/lib/styles/styles.css';
-
-const ENTERPRISE_SEARCH_BASE_URL = 'https://try-search-ui.ent.us-central1.gcp.cloud.es.io';
-const APP_SEARCH_SEARCH_KEY = 'search-eaesy8bpwje7u7jgtydgs846';
-const APP_SEARCH_ENGINE_NAME = 'national-parks-demo';
-
-const connectorConfiguration = {
-  endpointBase: ENTERPRISE_SEARCH_BASE_URL,
-  searchKey: APP_SEARCH_SEARCH_KEY,
-  engineName: APP_SEARCH_ENGINE_NAME,
-  cacheResponses: false,
-}
-
-const connector = new AppSearchAPIConnector(connectorConfiguration);
-
 function App() {
   return (
-    <SearchProvider config={{apiConnector: connector}}>
-      <div className="App">
-        <Layout header={<SearchBox />} bodyContent={<Results />} />
-      </div>
-    </SearchProvider>
+    <div className="App">
+    </div>
   );
 }
 
