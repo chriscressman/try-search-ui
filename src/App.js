95b3e8aa920bfabd19915e9734e6a608c9a53f43
Chris Cressman <chris@chriscressman.com>
2022-03-08 16:49:22 -0500

    Log exports from each module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import * as searchUi from '@elastic/search-ui'
+import * as searchUiAppSearchConnector from '@elastic/search-ui-app-search-connector'
+import * as reactSearchUi from '@elastic/react-search-ui'
+import * as reactSearchUiViews from '@elastic/react-search-ui-views'
+
 function App() {
+  console.log(
+    searchUi,
+    searchUiAppSearchConnector,
+    reactSearchUi,
+    reactSearchUiViews
+  );
   return (
     <div className="App">
     </div>
