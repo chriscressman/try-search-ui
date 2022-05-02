d8cdb07ccf2c4a202f0965c00119c459ccd58830
Chris Cressman <chris@chriscressman.com>
2022-05-02 15:47:19 -0400

    Explore latest Search UI releases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import * as searchUi from '@elastic/search-ui'
 import * as searchUiAppSearchConnector from '@elastic/search-ui-app-search-connector'
+import * as searchUiElasticsearchConnector from '@elastic/search-ui-elasticsearch-connector'
+import * as searchUiSiteSearchConnector from '@elastic/search-ui-site-search-connector'
+import * as searchUiWorkplaceSearchConnector from '@elastic/search-ui-workplace-search-connector'
 import * as reactSearchUi from '@elastic/react-search-ui'
 import * as reactSearchUiViews from '@elastic/react-search-ui-views'
 
@@ -7,6 +10,9 @@ function App() {
   console.log(
     searchUi,
     searchUiAppSearchConnector,
+    searchUiElasticsearchConnector,
+    searchUiSiteSearchConnector,
+    searchUiWorkplaceSearchConnector,
     reactSearchUi,
     reactSearchUiViews
   );
